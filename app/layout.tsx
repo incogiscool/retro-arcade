a6b0e70faf9fbf88b90412f828a48b219f7e6911
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-08-12 17:47:39 -0400

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { HomeNavbar } from "@/components/nav/HomeNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen flex gap-8">
+          <HomeNavbar className="h-full" />
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ components/nav/HomeNavbar.tsx
@@ -0,0 +1,5 @@
+import { cn } from "@/lib/utils";
+
+export const HomeNavbar = ({ className }: { className?: string }) => {
+  return <nav className={cn("h-full border w-[100px]", className)}>omak</nav>;
+};
