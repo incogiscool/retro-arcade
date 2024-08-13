fc258c7348e38782053206f4da118c5cfa9208a4
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-08-12 22:24:22 -0400

    update nav searchbar and layout

--- components/nav/HomeNavbar.tsx
+++ components/nav/HomeNavbar.tsx
@@ -3,13 +3,13 @@ import { Input } from "../ui/input";
 
 export const HomeNavbar = () => {
   return (
-    <nav className="w-full border-b h-fit p-4 border-border bg-background-secondary flex gap-4">
+    <nav className="w-full border-b h-fit p-4 border-border bg-background-secondary items-center flex gap-6">
       <p>Retro Arcade Games</p>
-      <div className="flex gap-1 items-center border border-border p-1 rounded-md">
+      <div className="flex gap-1 items-center border border-border w-[250px] px-2 rounded-md">
         <Search className="text-sm" />
         <Input
           placeholder="Search..."
-          className="bg-background-secondary w-full border-none outline-none ring-0"
+          className="bg-background-secondary w-full border-none outline-none focus:ring-0 ring-0"
         />
       </div>
     </nav>
