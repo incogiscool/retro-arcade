921a906196f2a15d2b64bca20d66600cb3caf1e2
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-08-12 21:59:26 -0400

    try and fix input

--- components/nav/HomeNavbar.tsx
+++ components/nav/HomeNavbar.tsx
@@ -1,9 +1,17 @@
+import { Search } from "lucide-react";
 import { Input } from "../ui/input";
 
 export const HomeNavbar = () => {
   return (
-    <nav className="w-full border-b h-fit bg-background-secondary">
-      <Input placeholder="Search..." />
+    <nav className="w-full border-b h-fit p-4 border-border bg-background-secondary flex gap-4">
+      <p>Retro Arcade Games</p>
+      <div className="flex gap-1 items-center border border-border p-1 rounded-md">
+        <Search className="text-sm" />
+        <Input
+          placeholder="Search..."
+          className="bg-background-secondary w-full border-none outline-none ring-0"
+        />
+      </div>
     </nav>
   );
 };
