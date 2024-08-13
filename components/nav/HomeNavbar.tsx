d2e6b1cd68af66caca530bee34cb0dada83e9b68
incogiscool <94598096+incogiscool@users.noreply.github.com>
2024-08-13 02:20:58 -0400

    fix input FINALLYYY + github icon

--- components/nav/HomeNavbar.tsx
+++ components/nav/HomeNavbar.tsx
@@ -1,17 +1,29 @@
-import { Search } from "lucide-react";
+import { Github, Search } from "lucide-react";
 import { Input } from "../ui/input";
 
 export const HomeNavbar = () => {
   return (
-    <nav className="w-full border-b h-fit p-4 border-border bg-background-secondary items-center flex gap-6">
-      <p>Retro Arcade Games</p>
-      <div className="flex gap-1 items-center border border-border w-[250px] px-2 rounded-md">
-        <Search className="text-sm" />
-        <Input
-          placeholder="Search..."
-          className="bg-background-secondary w-full border-none outline-none focus:ring-0 ring-0"
-        />
+    <nav className="w-full border-b h-fit p-4 border-border bg-background-secondary items-center flex gap-6 justify-between flex-wrap">
+      <div className="flex gap-6 items-center">
+        <p>Retro Arcade Games</p>
+        <div className="flex gap-1 items-center border border-border w-[250px] px-2 rounded-md">
+          <Search className="text-sm" />
+          <Input
+            placeholder="Search..."
+            className="bg-background-secondary w-full border-none"
+            noFocus
+          />
+        </div>
       </div>
+
+      <a
+        className="bg-background p-2 rounded-md border-border border hover:bg-background/80 transition"
+        href="https://github.com/incogiscool/retro-arcade"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Github />
+      </a>
     </nav>
   );
 };
